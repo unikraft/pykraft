0ee47f6ff11084be17ad4cb8ee90fdfdc4d71d04
Alexander Jung <alexander.jung@neclab.eu>
2020-05-06 18:04:20 +0200

    kraft/utils/cli: Clean up mutex error output
    Signed-off-by: Alexander Jung <alexander.jung@neclab.eu>

--- kraft/utils/cli.py
+++ kraft/utils/cli.py
@@ -185,15 +185,23 @@ class ClickOptionMutex(click.Option):
 
     def handle_parse_result(self, ctx, opts, args):
         current_opt = self.name in opts
+
+        # Find option CLI names
+        params = {}
+        for param in ctx.command.params:
+            params[param.name] = param.opts
+
         for mutex_opt in self.not_required_if:
             if mutex_opt in opts:
                 if current_opt:
                     raise click.UsageError(
-                        "Illegal usage: '" +
-                        str(self.name) +
-                        "' is mutually exclusive with " +
-                        str(mutex_opt) + "."
+                        "Illegal usage: " +
+                        '/'.join(params[str(self.name)]) +
+                        " is mutually exclusive with " +
+                        '/'.join(params[str(mutex_opt)]) + "."
                     )
+
                 else:
                     self.prompt = None
+
         return super(ClickOptionMutex, self).handle_parse_result(ctx, opts, args)
