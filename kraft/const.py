4580d80c677385b231baa67ae8a681d2eb2bbda2
Alexander Jung <alexander.jung@neclab.eu>
2020-09-29 18:40:50 +0200

    const: Rename and introduce new constants.
    Signed-off-by: Alexander Jung <alexander.jung@neclab.eu>

--- kraft/const.py
+++ kraft/const.py
@@ -2,7 +2,8 @@
 #
 # Authors: Alexander Jung <alexander.jung@neclab.eu>
 #
-# Copyright (c) 2020, NEC Europe Ltd., NEC Corporation. All rights reserved.
+# Copyright (c) 2020, NEC Europe Laboratories GmbH., NEC Corporation.
+#                     All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -35,14 +36,20 @@ import re
 
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-BRANCH_MASTER = "master"
-BRANCH_STAGING = "staging"
+UNIKRAFT_RELEASE_STABLE = "stable"
+UNIKRAFT_RELEASE_STAGING = "staging"
+UNIKRAFT_RELEASE_BRANCHES = {
+    UNIKRAFT_RELEASE_STABLE: "master",
+    UNIKRAFT_RELEASE_STAGING: "staging"
+}
 
 UK_GITHUB_ORG = 'unikraft'
 
 # Match against dereferenced tags only
 # https://stackoverflow.com/a/15472310
-GIT_UNIKRAFT_TAG_PATTERN = re.compile(r'refs/tags/RELEASE-([\d\.]+)\^\{\}')
+GIT_UNIKRAFT_TAG_PATTERN = re.compile(r'RELEASE-([\d\.]+)')
+GIT_UNIKRAFT_TAG_RELEASE = 'RELEASE-%s'
+# GIT_UNIKRAFT_TAG_PATTERN = re.compile(r'refs/tags/RELEASE-([\d\.]+)\^\{\}')
 GIT_TAG_PATTERN = re.compile(r'refs/tags/([\w\d\.-]+)[\^\{\}]?')
 GIT_BRANCH_PATTERN = re.compile(r'refs/heads/(.*)')
 VSEMVER_PATTERN = re.compile(r'^v\d')
@@ -65,13 +72,14 @@ SEMVER_PATTERN = re.compile(
     re.VERBOSE,
 )
 
-GITHUB_ORIGIN = "https://github.com"
+GITHUB_ORIGIN = "github.com"
+GITHUB_TARBALL = "https://github.com/%s/%s/archive/%s.tar.gz"
 UNIKRAFT_ORG = "unikraft"
 UNIKRAFT_CORE = "%s/%s/%s" % (GITHUB_ORIGIN, UNIKRAFT_ORG, "unikraft.git")
 UNIKRAFT_ORIGIN = "%s/%s" % (GITHUB_ORIGIN, UNIKRAFT_ORG)
 
 REPO_VERSION_DELIMETERE = "@"
-ORG_DELIMETERE = "/"
+ORG_DELIMETER = "/"
 
 REPO_VALID_URL_PREFIXES = (
     'http://',
@@ -111,8 +119,11 @@ MAKEFILE_UK = "Makefile.uk"
 CONFIG_UK = "Config.uk"
 ENV_VAR_PATTERN = re.compile(r'([A-Z_^=]+)=(\'[/\w\.\-\s]+\')')
 
+UNIKRAFT_CACHEDIR = ".kraftcache"
 UNIKRAFT_WORKDIR = ".unikraft"
 UNIKRAFT_COREDIR = "unikraft"
+UNIKRAFT_ARCHSDIR = "archs"
+UNIKRAFT_PLATSDIR = "plats"
 UNIKRAFT_LIBSDIR = "libs"
 UNIKRAFT_APPSDIR = "apps"
 
@@ -129,13 +140,14 @@ GITCONFIG_GLOBAL = "~/.gitconfig"
 GITCONFIG_LOCAL = ".git/config"
 URL_VERSION = '$VERSION'
 
-KRAFTCONF = ".kraftrc"
-KRAFTCONF_DELIMETER = "/"
-KRAFTCONF_INIT_WORKDIR = "init/workdir"
-KRAFTCONF_CONFIGURE_PLATFORM = "configure/platform"
-KRAFTCONF_CONFIGURE_ARCHITECTURE = "configure/architecture"
+KRAFTRC = ".kraftrc"
+KRAFTRC_DELIMETER = "/"
+KRAFTRC_LIST_ORIGINS = "list/origins"
+KRAFTRC_INIT_WORKDIR = "init/workdir"
+KRAFTRC_CONFIGURE_PLATFORM = "configure/platform"
+KRAFTRC_CONFIGURE_ARCHITECTURE = "configure/architecture"
 
-KCONFIG = 'CONFIG_%s'
+KCONFIG = "CONFIG_%s"
 KCONFIG_Y = 'y'
 KCONFIG_N = 'n'
 KCONFIG_EQ = '%s=%s'
@@ -143,9 +155,20 @@ KCONFIG_ARCH_NAME = "CONFIG_ARCH_%s"
 KCONFIG_PLAT_NAME = "CONFIG_PLAT_%s"
 KCONFIG_LIB_NAME = "CONFIG_LIB%s"
 
-UK_CORE_ARCH_DIR = '%s/arch'
-UK_CORE_PLAT_DIR = '%s/plat'
+UK_CORE_ARCH_DIR = "%s/arch/%s"
+UK_CORE_PLAT_DIR = "%s/plat/%s"
 UK_VERSION_VARNAME = '$(%s_VERSION)'
+
+UK_CORE_ARCHS = [
+    'x86_64',
+    'arm64',
+    'arm',
+]
+UK_CORE_PLATS = [
+    'kvm',
+    'xen',
+    'linuxu',
+]
 
 CONFIG_UK_ARCH = re.compile(
     r"""
@@ -157,7 +180,7 @@ CONFIG_UK_PLAT = re.compile(r'menuconfig\s+([\w\_]+)')
 CONFIG_UK_LIB = re.compile(r'config\s+([\w\_]+)')
 
 UK_GITHUB_NAMING_FORMAT = r'(%s)-([^.]+)'
-UK_GITHUB_CORE_FORMAT = re.compile(r'(unikraft)/(unikraft)')
+UK_GITHUB_CORE_FORMAT = re.compile(r'(unikraft)')
 
 UK_COMPAT_CORE_v0_4_0 = "0.4.0"
 
@@ -168,7 +191,11 @@ UK_DBG_EXT = '.dbg'
 TMPL_EXT = '.tmpl'
 
 TEMPLATE_CONFIG = 'cookiecutter.json'
+TEMPLATE_LIB = 'lib'
+TEMPLATE_APP = 'app'
 TEMPLATE_MANIFEST = 'manifest.yaml'
 
 QEMU_GUEST = 'qemu-guest'
 XEN_GUEST = 'xen-guest'
+
+LIST_DESC_WIDTH = 50
