567a1daf255ddadb681ff53cc5683a94640f3916
Alexander Jung <alexander.jung@neclab.eu>
2020-09-29 18:46:01 +0200

    config/interpolation: No longer interpolate the version in the source.
    Signed-off-by: Alexander Jung <alexander.jung@neclab.eu>

--- kraft/config/interpolation.py
+++ kraft/config/interpolation.py
@@ -2,7 +2,8 @@
 #
 # Authors: Alexander Jung <alexander.jung@neclab.eu>
 #
-# Copyright (c) 2020, NEC Europe Ltd., NEC Corporation. All rights reserved.
+# Copyright (c) 2020, NEC Europe Laboratories GmbH., NEC Corporation.
+#                     All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -36,18 +37,9 @@ from string import Template
 
 import six
 
-from kraft.components.types import RepositoryType
-from kraft.constants import BRANCH_MASTER
-from kraft.constants import GITHUB_ORIGIN
-from kraft.constants import ORG_DELIMETERE
-from kraft.constants import REPO_VALID_URL_PREFIXES
-from kraft.constants import REPO_VERSION_DELIMETERE
-from kraft.constants import UNIKRAFT_CORE
-from kraft.constants import UNIKRAFT_ORG
-from kraft.errors import ConfigurationError
-from kraft.errors import InvalidInterpolation
-from kraft.errors import InvalidRepositorySource
-from kraft.errors import UnsetRequiredSubstitution
+from kraft.error import ConfigurationError
+from kraft.error import InvalidInterpolation
+from kraft.error import UnsetRequiredSubstitution
 
 
 class Interpolator(object):
@@ -188,87 +180,6 @@ def interpolate_environment_variables(version, config, section, environment):
             (name, process_item(name, config_dict or {}))
             for name, config_dict in config.items()
         )
-
-
-def interpolate_source_version(name=None,  # noqa: C901
-                               source=None,
-                               version=None,
-                               repository_type=None):
-    """Parse a well-known repository naming format such that a Repository
-    object is realized.
-
-    The general semantics for a source string are:
-
-        <ORIGIN>@<BRANCH, TAG or COMMIT HASH>.
-
-    This allows for a fully qualified source origin, for example:
-
-        git://git@github.com/unikraft/unikraft.git@v0.4.0
-        https://github.com/unikraft/unikraft.git@v0.4.0
-        file:///root/unikraft/libs/my-lib@staging
-
-    Additionally, an abbreviated source can also be provided such that its
-    origin is inferred from Unikraft's Github organization.  This means
-    providing the following will qualify as valid and inferred,
-    respectively, sources:
-
-        - v0.4.0 -> https://github.com.unikraft/unikraft.git@v0.4.0
-        - lib-lwip -> https://github.com.unikraft/lib-lwip.git@master
-        - lib-lwip@v0.4.0 -> https://github.com.unikraft/lib-lwip.git@v0.4.0
-        - unikraft/lib-lwip -> https://github.com.unikraft/lib-lwip.git@master
-        - unikraft/lib-lwip@staging -> https://github.com/unikraft/lib-lwip.git@staging
-    """
-
-    if source is None:
-        if version is None:
-            raise InvalidRepositorySource(source)
-        else:
-            source = version
-            version = None
-
-    # First determine if the provided string has a version
-    if REPO_VERSION_DELIMETERE in source:
-        source, version = source.split(REPO_VERSION_DELIMETERE, 1)
-
-        # The source could be accessible via ssh (user@host), remedy this:
-        if REPO_VERSION_DELIMETERE in version:
-            x, version = version.split(REPO_VERSION_DELIMETERE, 1)
-            source = source + REPO_VERSION_DELIMETERE + x
-
-    # If it's not a valid URL source, we must begin to make assumptions
-    if not source.startswith(REPO_VALID_URL_PREFIXES):
-
-        # Check if a reference to a Github org/repo:
-        if ORG_DELIMETERE in source:
-            source = "%s/%s" % (GITHUB_ORIGIN, source)
-
-        # Does the repository start with lib-, plat-, etc.?
-        elif source.startswith(tuple(y.shortname + '-' for x, y in RepositoryType.__members__.items())):
-            source = "%s/%s/%s" % (GITHUB_ORIGIN, UNIKRAFT_ORG, source)
-
-        # Can we inference anything from its name (which will have been
-        # retrieved from the configuration prefix, e.g. my-lib: version)
-        elif name is not None and repository_type is RepositoryType.LIB:
-            version = source
-            source = "%s/%s/lib-%s" % (GITHUB_ORIGIN, UNIKRAFT_ORG, name)
-
-        # Assuming it's a version tag for the main unikraft repo:
-        elif version is None:
-            version = source
-            source = UNIKRAFT_CORE
-
-        # Of the format "unikraft@version"
-        elif source == UNIKRAFT_ORG:
-            source = UNIKRAFT_CORE
-
-        else:
-            raise InvalidRepositorySource(source)
-
-    # Still no version?  Try master
-    if version is None:
-        version = BRANCH_MASTER
-
-    return source, version
 
 
 class ConversionMap(object):
