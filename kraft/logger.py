5afd89433e1d84a7b34075e717d01b89429bbb78
Alexander Jung <alexander.jung@neclab.eu>
2020-09-29 18:44:02 +0200

    logger: Improve logger output with colors!
    Signed-off-by: Alexander Jung <alexander.jung@neclab.eu>

--- kraft/logger.py
+++ kraft/logger.py
@@ -2,7 +2,8 @@
 #
 # Authors: Alexander Jung <alexander.jung@neclab.eu>
 #
-# Copyright (c) 2020, NEC Europe Ltd., NEC Corporation. All rights reserved.
+# Copyright (c) 2020, NEC Europe Laboratories GmbH., NEC Corporation.
+#                     All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -32,13 +33,85 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import logging
+from logging import DEBUG
+from logging import PercentStyle
+
+import click
 
 from kraft import __program__
 
-logging.basicConfig(
-    level=logging.ERROR,
-    format="%(asctime)s %(levelname)-8s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
-)
+TIME_FORMAT = "%(asctime)s "
+LEVEL_FORMAT = "%(levelname)-8s"
+MESSAGE_FORMAT = "%(message)s"
+LOGGING_COLORS = {
+    'WARNING':  'yellow',
+    'INFO':     'white',
+    'DEBUG':    'cyan',
+    'CRITICAL': 'red',
+    'ERROR':    'red'
+}
 
+class KraftFormatter(logging.Formatter):
+    _use_color = False
+    _logger = None
+
+    def __init__(self, *args, **kwargs):
+        self._logger = kwargs.get("logger")
+        if self._logger is not None:
+            del kwargs["logger"]
+
+        logging.Formatter.__init__(self, *args, **kwargs)
+
+    def format(self, record):
+        fmt = ""
+
+        if self._logger.use_timestamps:
+            fmt += TIME_FORMAT
+
+        fmt += "["
+        if self._logger.use_color:
+            fmt += click.style(LEVEL_FORMAT, fg=LOGGING_COLORS[record.levelname])
+        else:
+            fmt += LEVEL_FORMAT
+        fmt += "] "
+
+        fmt += MESSAGE_FORMAT
+
+
+        self._style._fmt = fmt
+
+        return logging.Formatter.format(self, record)
+
+class KraftLogger(logging.Logger):
+    _use_timestamps = False
+    @property
+    def use_timestamps(self): return self._use_timestamps
+
+    @use_timestamps.setter
+    def use_timestamps(self, use_timestamps=False):
+        self._use_timestamps = use_timestamps
+
+    _use_color = True
+    @property
+    def use_color(self): return self._use_color
+
+    @use_color.setter
+    def use_color(self, use_color=False):
+        self._use_color = use_color
+
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.ERROR)
+
+        color_formatter = KraftFormatter(
+            logger=self,
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+
+        console = logging.StreamHandler()
+        console.setFormatter(color_formatter)
+
+        self.addHandler(console)
+
+
+logging.setLoggerClass(KraftLogger)
 logger = logging.getLogger(__program__)
