405ad78628e2877e9fc11b4aecbd18fcd6c92bec
Alexander Jung <alexander.jung@neclab.eu>
2020-09-29 18:47:20 +0200

    config/environment: Lint fixes and missing import fixes.
    Signed-off-by: Alexander Jung <alexander.jung@neclab.eu>

--- kraft/config/environment.py
+++ kraft/config/environment.py
@@ -2,7 +2,8 @@
 #
 # Authors: Alexander Jung <alexander.jung@neclab.eu>
 #
-# Copyright (c) 2020, NEC Europe Ltd., NEC Corporation. All rights reserved.
+# Copyright (c) 2020, NEC Europe Laboratories GmbH., NEC Corporation.
+#                     All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -37,23 +38,28 @@ import re
 import dotenv
 import six
 
-from kraft.errors import ConfigurationError
-from kraft.errors import EnvFileNotFound
+from kraft.error import ConfigurationError
+from kraft.error import EnvFileNotFound
 from kraft.logger import logger
 
 
 def split_env(env):
     if isinstance(env, six.binary_type):
         env = env.decode('utf-8', 'replace')
+
     key = value = None
+
     if '=' in env:
         key, value = env.split('=', 1)
+
     else:
         key = env
+
     if re.search(r'\s', key):
         raise ConfigurationError(
             "environment variable name '{}' may not contain whitespace.".format(key)
         )
+
     return key, value
 
 
@@ -61,14 +67,22 @@ def env_vars_from_file(filename, interpolate=True):
     """
     Read in a line delimited file of environment variables.
     """
+
     if not os.path.exists(filename):
         raise EnvFileNotFound("Couldn't find env file: {}".format(filename))
+
     elif not os.path.isfile(filename):
         raise EnvFileNotFound("{} is not a file.".format(filename))
 
-    env = dotenv.dotenv_values(dotenv_path=filename, encoding='utf-8-sig', interpolate=interpolate)
+    env = dotenv.dotenv_values(
+        dotenv_path=filename,
+        encoding='utf-8-sig',
+        interpolate=interpolate
+    )
+
     for k, v in env.items():
         env[k] = v if interpolate else v.replace('$', '$$')
+
     return env
 
 
@@ -82,49 +96,62 @@ class Environment(dict):
     def from_env_file(cls, base_dir, env_file=None):
         def _initialize():
             result = cls()
+
             if base_dir is None:
                 return result
+
             if env_file:
                 env_file_path = os.path.join(base_dir, env_file)
+
             else:
                 env_file_path = os.path.join(base_dir, '.env')
+
             try:
                 return cls(env_vars_from_file(env_file_path))
+
             except EnvFileNotFound:
                 pass
+
             return result
 
         instance = _initialize()
         instance.update(os.environ)
+
         return instance
 
     @classmethod
     def from_command_line(cls, parsed_env_opts):
         result = cls()
+
         for k, v in parsed_env_opts.items():
             # Values from the command line take priority, unless they're unset
             # in which case they take the value from the system's environment
             if v is None and k in os.environ:
                 result[k] = os.environ[k]
+
             else:
                 result[k] = v
+
         return result
 
     def __getitem__(self, key):
         try:
             return super(Environment, self).__getitem__(key)
+
         except KeyError:
             if not self.silent and key not in self.missing_keys:
                 logger.warning(
                     "The %s variable is not set. Defaulting to a blank string."
                     % key
                 )
+
                 self.missing_keys.append(key)
 
             return ""
 
     def __contains__(self, key):
         result = super(Environment, self).__contains__(key)
+
         return result
 
     def get(self, key, *args, **kwargs):
@@ -135,8 +162,11 @@ class Environment(dict):
         # Unset, empty, "0" and "false" (i-case) yield False.
         # All other values yield True.
         value = self.get(key)
+
         if not value:
             return False
+
         if value.lower() in ['0', 'false']:
             return False
+
         return True
