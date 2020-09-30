d7eca3fef2a2f4c31f0e59bd220f63f3a8375481
Alexander Jung <alexander.jung@neclab.eu>
2020-09-30 19:20:43 +0200

    lib: Export module.
    Signed-off-by: Alexander Jung <alexander.jung@neclab.eu>

---
+++ kraft/cmd/list/__init__.py
@@ -0,0 +1,58 @@
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Authors: Alexander Jung <alexander.jung@neclab.eu>
+#
+# Copyright (c) 2020, NEC Europe Laboratories GmbH., NEC Corporation.
+#                     All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# flake8: noqa
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from .list import kraft_list_preflight
+
+from .list import cmd_list
+from .add import cmd_list_add
+from .remove import cmd_list_remove
+from .pull import cmd_list_pull
+from .update import cmd_list_update
+from .show import cmd_list_show
+
+from .pull import kraft_download_component
+
+from kraft.manifest import ManifestIndex
+from kraft.manifest import Manifest
+from kraft.manifest import ManifestItem
+from kraft.manifest import ManifestItemVersion
+from kraft.manifest import ManifestItemDistribution
+
+
+cmd_list.add_command(cmd_list_add)
+cmd_list.add_command(cmd_list_remove)
+cmd_list.add_command(cmd_list_pull)
+cmd_list.add_command(cmd_list_update)
+cmd_list.add_command(cmd_list_show)
