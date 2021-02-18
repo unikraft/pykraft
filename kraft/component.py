14f73ca05935fbf05f4dbc18cb44578c7bf1870d
Alexander Jung <a.jung@lancs.ac.uk>
2021-02-18 10:50:35 +0100

    coomponent: Remove workdir attribute
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

--- kraft/component.py
+++ kraft/component.py
@@ -96,11 +96,6 @@ class Component(object):
     @property
     def description(self): return self._description
 
-    _workdir = None
-
-    @property
-    def workdir(self): return self._workdir
-
     _localdir = None
 
     @property
@@ -146,7 +141,6 @@ class Component(object):
         self._type = kwargs.get("type", None)
         self._manifest = kwargs.get("manifest", None)
         self._localdir = kwargs.get("localdir", None)
-        self._workdir = kwargs.get("workdir", None)
         ignore_version = kwargs.get("ignore_version", False)
         self._kconfig = list()
 
