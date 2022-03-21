unikraft/pykraft

commit 1c200bf5d8de64f9808d40bc8def7333a5f0bd55

A tricky fix for binname/appname not matched bug (#80 and #83).
Signed-off-by: Xiangyi Meng <xymeng16@gmail.com>

kraft/app/app.py
@@ -355,6 +355,10 @@ class Application(Component):
         try:
             return_code = self.make([
                 ('UK_DEFCONFIG=%s' % path),
+                # see #80 and #83, the fallback behavior once name field is missing in the original kraft.yaml
+                # is to use the parent folder name, and the missing field will be automatically complemented
+                # (ensured by config.load_config() implementation)
+                ('CONFIG_UK_NAME=%s' % self.config.name),
                 'defconfig'
             ])
         finally:

commit fce7d54446f27f460f83f6c93e87c1ebb30f71b3

kraft: Fix linting issue introduced in 1c200bf
Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

kraft/app/app.py
@@ -362,9 +362,9 @@ class Application(Component):
         try:
             return_code = self.make([
                 ('UK_DEFCONFIG=%s' % path),
-                # see #80 and #83, the fallback behavior once name field is missing in the original kraft.yaml
-                # is to use the parent folder name, and the missing field will be automatically complemented
-                # (ensured by config.load_config() implementation)
+                # see #80 and #83, the fallback behavior once name field is missing in the original
+                # kraft.yaml is to use the parent folder name, and the missing field will be
+                # automatically complemented (ensured by config.load_config() implementation)
                 ('CONFIG_UK_NAME=%s' % self.config.name),
                 'defconfig'
             ])